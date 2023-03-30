8c4f9e9d3347d081a0c97272e8936f26c3a072e4
Posvian <posvianski@mail.ru>
2023-03-30 20:43:59 +0200

    made hometask

---
+++ flaskProject/blog/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+from blog.article.views import article
+from blog.user.views import user
+
+
+def create_app():
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(user)
+    app.register_blueprint(article)
